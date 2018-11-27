109f5025c2cc7f8d503bd7b6740308dc7896e911
Cross Smith <precsmit@ut.utm.edu>
2018-11-26 22:07:15 -0600

    Now finds sum

--- main.cpp
+++ main.cpp
@@ -2,10 +2,29 @@
 
 using namespace std;
 
+int sum(int n);
+
 int main() {
 
-  cout << "Hello World!" << endl;
+  int n;
+
+  cout << "Please enter a number: ";
+  cin >> n;
+
+  cout << "The sum of the numbers from 1 to " << n << " is " << sum(n) << endl;
 
   return 0;
+
+}
+
+int sum(int n) {
+
+  int value = 0;
+
+  for (int i = 1; i <= n; i++) {
+    value += i;
+  }
+
+  return value;
 
 }
